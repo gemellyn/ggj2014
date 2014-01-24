ab63d97671f360cf113873ada5e38b91e9cc77a0
RedNaK <rednak@interaction-project.net>
2014-01-25 00:23:46 +0100

    clean script + ajout folders + début scripts

---
+++ lui/Assets/Scripts/PickableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickableObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void function OnTriggerEnter (other : Collider) {
+		Debug.Log ("Enter", other.gameObject);
+	}
+}
