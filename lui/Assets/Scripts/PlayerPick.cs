cc9d609eba3f1dc4221fd626b778ca5fac8741fd
RedNaK <rednak@interaction-project.net>
2014-01-25 23:34:02 +0100

    pickable usability

--- lui/Assets/Scripts/PlayerPick.cs
+++ lui/Assets/Scripts/PlayerPick.cs
@@ -6,6 +6,9 @@ public class PlayerPick : MonoBehaviour {
 	private GameObject hitObject ;
 	private RaycastHit hit ;
 
+    private Transform PP;
+    private PickableObject po;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,25 +16,39 @@ public class PlayerPick : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey("e")) {
+		if(Input.GetKeyDown("e")) {
+
             //Debug.DrawLine(Camera.main.transform.position, Camera.main.transform.position+Camera.main.transform.forward * 10.0f, Color.red);
-			if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100)){
-                //Debug.Log(hit.collider.gameObject.name) ;
-                Transform PP = hit.collider.gameObject.transform.Find("PickableProximity") ;
-				if(PP){
-					PickableObject po = PP.GetComponent("PickableObject") as PickableObject ;
-					if(hit.collider.gameObject.collider.tag == "Pick" && po.pickable){
-						hitObject = hit.collider.gameObject ;
-						hitObject.transform.rigidbody.isKinematic = true;
-                        hitObject.collider.enabled = false;
-                        hitObject.transform.parent.transform.parent = transform;
-                        hitObject.transform.parent.transform.position = new Vector3(hitObject.transform.parent.transform.position.x, transform.position.y + 1.0f, hitObject.transform.parent.transform.position.z);
-                        po.picked = true ;
+            if (hitObject != null){
+                hitObject.transform.parent.transform.parent = null;
+                hitObject.transform.parent.transform.position = transform.position + transform.forward;
+                hitObject.transform.rigidbody.isKinematic = false;
+                hitObject.collider.enabled = true;
+                hitObject = null;
+                po.picked = false;
+            }
+            else{
+                if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 100))
+                {
+                    //Debug.Log(hit.collider.gameObject.name) ;
+                    PP = hit.collider.gameObject.transform.Find("PickableProximity");
+                    if (PP)
+                    {
+                        po = PP.GetComponent("PickableObject") as PickableObject;
+                        if (hit.collider.gameObject.collider.tag == "Pick" && po.pickable)
+                        {
+                            hitObject = hit.collider.gameObject;
+                            hitObject.transform.rigidbody.isKinematic = true;
+                            hitObject.collider.enabled = false;
+                            hitObject.transform.parent.transform.parent = transform;
+                            hitObject.transform.parent.transform.position = new Vector3(hitObject.transform.parent.transform.position.x, transform.position.y + 1.0f, hitObject.transform.parent.transform.position.z);
+                            po.picked = true;
+                        }
                     }
-				}
-			}
+                }
+            }
 		}
-		
+		/*
 		if(Input.GetKey("f")&& hitObject != null){
             Transform PP = hitObject.transform.Find("PickableProximity");
             PickableObject po = PP.GetComponent("PickableObject") as PickableObject;
@@ -41,7 +58,7 @@ public class PlayerPick : MonoBehaviour {
             hitObject.collider.enabled = true;
 			hitObject = null;
             po.picked = false;
-		}
+		}*/
 
 	}
 
