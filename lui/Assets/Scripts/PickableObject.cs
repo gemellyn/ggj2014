cad8c8f6ff175d5d513b7e93da6d3e5a24b722a5
RedNaK <rednak@interaction-project.net>
2014-01-25 04:42:40 +0100

    pick ok

--- lui/Assets/Scripts/PickableObject.cs
+++ lui/Assets/Scripts/PickableObject.cs
@@ -4,31 +4,27 @@ using System.Collections;
 public class PickableObject : MonoBehaviour {
 
 	private GameObject objectToPick ;
-	public Material shineMat ;
+
+    public float delight ;
+
+	[HideInInspector]
 	public Material oldMat ;
-	private bool picked = false ;
-	private Camera mainCam ;
+	[HideInInspector]
 	public bool pickable = false ;
 
-	void Start(){
-		mainCam = Camera.main;
-	}
 
 	void OnTriggerEnter(Collider other) {
-		Debug.Log (other.gameObject.name);
 		if (other.gameObject.name == "player") {
-			Debug.Log("Player in trigger");
 			objectToPick = transform.parent.gameObject ;
 			if(oldMat == null){
 				oldMat = objectToPick.renderer.materials[0] ; 
 			}
-			objectToPick.renderer.material = shineMat ;
+			objectToPick.renderer.material =  Resources.Load("Pickable") as Material ;
 			this.pickable = true ;
 		}
 	}
 	void OnTriggerExit(Collider other) {
 		if (other.gameObject.name == "player") {
-			Debug.Log("Player out of trigger");
 			objectToPick.renderer.material = oldMat ;
 			this.pickable = false ;
 		}
--- lui/Assets/Scripts/PlayerPick.cs
+++ lui/Assets/Scripts/PlayerPick.cs
@@ -5,6 +5,8 @@ public class PlayerPick : MonoBehaviour {
 
 	private GameObject hitObject ;
 	private RaycastHit hit ;
+	private bool show = false ;
+	private float initialX ;
 
 	// Use this for initialization
 	void Start () {
@@ -14,14 +16,13 @@ public class PlayerPick : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey("e")) {
-			if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 500)){
-				Transform t = hit.collider.gameObject.transform.Find("PickableProximity") ;
-				if(t){
-					Debug.Log(hit.collider.gameObject.name) ;
-					PickableObject po = t.GetComponent("PickableObject") as PickableObject ;
+			if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 50)){
+				Transform PP = hit.collider.gameObject.transform.Find("PickableProximity") ;
+				if(PP){
+					PickableObject po = PP.GetComponent("PickableObject") as PickableObject ;
 					if(hit.collider.gameObject.collider.tag == "Pick" && po.pickable){
-						Debug.Log("Pick it please "+hit.collider.gameObject.name) ;
 						hitObject = hit.collider.gameObject ;
+						hitObject.transform.rigidbody.isKinematic = true;
 					}
 				}
 			}
@@ -29,12 +30,28 @@ public class PlayerPick : MonoBehaviour {
 
 		if (hitObject != null) {
 			hitObject.transform.parent.transform.parent = gameObject.transform ;
-			hitObject.transform.position = new Vector3(hitObject.transform.position.x, 0.3f, hitObject.transform.position.z) ;
+			float y = 0.3f ;
+			/*if(show){
+				y -= 0.15f ;
+			}*/
+			hitObject.transform.position = new Vector3(hitObject.transform.position.x, y, hitObject.transform.position.z) ;
 		}
+        /*
+		if (Input.GetMouseButtonDown (0) && hitObject != null) {
+            hitObject.transform.position = new Vector3(hitObject.transform.position.x + (transform.forward.x * 0.5f), 0.15f, hitObject.transform.position.z);
+			show = true ;
+
+		}
+
+		if (show && Input.GetMouseButtonUp (0) && hitObject != null) {
+            hitObject.transform.position = new Vector3(hitObject.transform.position.x - (transform.forward.x * 0.5f), 0.3f, hitObject.transform.position.z);
+			show = false ;
+		}*/
 		
-		if(Input.GetKey("f")&& hitObject != null){
+		if(!Input.GetMouseButtonDown (0) && Input.GetKey("f")&& hitObject != null){
 			hitObject.transform.parent.transform.parent = null;
-			hitObject.transform.parent.transform.position = gameObject.transform.position ;
+			hitObject.transform.parent.transform.position = transform.position + transform.forward   ;
+			hitObject.transform.rigidbody.isKinematic = false;
 			hitObject = null;
 		}
 	}
